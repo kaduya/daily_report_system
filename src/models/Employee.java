e3bc0f4cb2b486af9d1558098862ebcf3afc6d18
kaduya <kaduya.t@gmail.com>
2021-06-25 22:32:04 +0900

    Add Employee index

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Table(name = "employees")
